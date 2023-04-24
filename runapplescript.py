be0b144af0e1c00dfefe7a80d2c76d4a07aa3523
S.Yugo <105782424+yugosuz@users.noreply.github.com>
2023-04-24 21:09:28 +0900

    コード追加

---
+++ runapplescript.py
@@ -0,0 +1,29 @@
+import serial
+import atexit
+import subprocess
+import sys
+import os
+
+ser = serial.Serial("シリアルポート名", 115200)
+
+def all_done():
+    # 終了時の処理
+    print("\nDone!")
+    ser.close()
+
+def main():
+    try:
+        while(True):
+            line = ser.readline().strip().decode('utf-8')
+            # 何かしらのトリガーがあったらスクリプトを実行する。
+            # 今回はシリアルモニタから"detected"という文字列が送られてきたら実行する。
+            if line == "detected":
+                subprocess.run(["osascript", "/path/to/stopyoutube.scpt"])
+                print("stopped!")
+
+    except KeyboardInterrupt:
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
